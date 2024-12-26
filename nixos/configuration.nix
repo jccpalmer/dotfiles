c801d9e937638f2ca0233cdd735a344960d5ec81
JC Palmer <me@jccpalmer.com>
2024-12-25 21:51:47 -0500

    Updating Linux kernel settings in configuation.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -11,6 +11,9 @@
       inputs.home-manager.nixosModules.default
     ];
 
+  # Linux kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
